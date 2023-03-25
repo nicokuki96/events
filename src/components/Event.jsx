588a6a14b767b5faba9067476f9d09e3bd8c31dd
nicokuki96 <72584691+nicokuki96@users.noreply.github.com>
2023-03-25 01:52:18 -0300

    Create a React app and publish it to GitHub Pages

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -15,50 +15,52 @@ import uuid from "react-uuid";
 import Grid from "@mui/material/Unstable_Grid2";
 
 const Event = (props) => {
-  const { event } = props;
-
+  const { event, imageUrls } = props;
   return (
     event &&
     event.map((item) => {
+      const imageUrl = imageUrls.find((i) => i.id === item.id)?.url;
       return (
-        <Card key={uuid()} sx={{ maxWidth: 345 }}>
-          <CardHeader
-            avatar={
-              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                R
-              </Avatar>
-            }
-            action={
-              <IconButton aria-label="settings">
-                <MoreVertIcon />
+        <Grid key={uuid()} item sm={12} md={3}>
+          <Card sx={{ maxWidth: 300 }}>
+            <CardHeader
+              avatar={
+                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                  R
+                </Avatar>
+              }
+              action={
+                <IconButton aria-label="settings">
+                  <MoreVertIcon />
+                </IconButton>
+              }
+              title={item.title}
+              subheader={`${item.date} - ${item.hour}`}
+            />
+            <CardMedia
+              component="img"
+              height="194"
+              image={imageUrl}
+              alt={imageUrl}
+            />
+            <CardContent>
+              <Typography variant="body2" color="text.secondary">
+                {item.description}
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <IconButton aria-label="add to favorites">
+                <FavoriteIcon />
               </IconButton>
-            }
-            title={item.title}
-            subheader={`${item.date} - ${item.hour}`}
-          />
-          <CardMedia
-            component="img"
-            height="194"
-            image={item.image}
-            alt="Paella dish"
-          />
-          <CardContent>
-            <Typography variant="body2" color="text.secondary">
-              {item.description}
-            </Typography>
-          </CardContent>
-          <CardActions disableSpacing>
-            <IconButton aria-label="add to favorites">
-              <FavoriteIcon />
-            </IconButton>
-            <IconButton aria-label="share">
-              <ShareIcon />
-            </IconButton>
-            <Grid xs display="flex" justifyContent="end" alignItems="center">
-              <Avatar src="/static/images/avatar/1.jpg" />
-            </Grid>
-          </CardActions>
-        </Card>
+              <IconButton aria-label="share">
+                <ShareIcon />
+              </IconButton>
+              <Grid xs display="flex" justifyContent="end" alignItems="center">
+                <Avatar src="/static/images/avatar/1.jpg" />
+              </Grid>
+            </CardActions>
+          </Card>
+        </Grid>
       );
     })
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,15 @@ import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
 import Event from "../components/Event";
 import Grid from "@mui/material/Grid";
+import { storage } from "../firebase";
+import { Container } from "@mui/material";
 
 const Home = (props) => {
-  const { userLog, getEvents } = useAuth();
+  const { userLog, getEvents, imageEvent } = useAuth();
   const { setOpen, open } = props;
   const [event, setEvent] = useState([]);
+  const [imageUrls, setImageUrls] = useState([]);
+  const [imageFile, setImageFile] = useState([]);
   const Alert = React.forwardRef(function Alert(props, ref) {
     return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
   });
@@ -21,12 +25,33 @@ const Home = (props) => {
     setOpen(false);
   };
 
-  useEffect(() => {
-    getEvents().then((data) => {
-      setEvent(data.docs.map((doc) => doc.data()));
+  // const convertUrlsToBlobs = async (imageUrls) => {
+  //   const imageBlobs = [];
+  //   for (const imageUrl of imageUrls) {
+  //     const response = await fetch(imageUrl);
+  //     const blob = await response.blob();
+  //     imageBlobs.push(blob);
+  //   }
+  //   return imageBlobs;
+  // };
 
-      console.log(event);
-    });
+  useEffect(() => {
+    getEvents()
+      .then((data) => {
+        setEvent(data.docs.map((doc) => doc.data()));
+      })
+      .then(() => {
+        const images = event.map(async (item) => {
+          const imageRef = storage.child(item.image);
+          return imageRef
+            .getDownloadURL()
+            .then((url) => ({ id: item.id, url }))
+            .catch(() => ({ id: item.id, url: null }));
+        });
+        Promise.all(images).then((result) => {
+          setImageUrls(result);
+        });
+      });
   }, []);
 
   return (
@@ -43,18 +68,11 @@ const Home = (props) => {
           </Alert>
         </Snackbar>
       )}
-
-      <Grid
-        container
-        direction="row"
-        justifyContent="flex-start"
-        alignItems="flex-start"
-        mt={2}
-        gap={1}
-        className="eventBox"
-      >
-        <Event event={event} />
-      </Grid>
+      <Container>
+        <Grid className="eventBox" mt={5} gap={2} container>
+          <Event event={event} imageUrls={imageUrls} />
+        </Grid>
+      </Container>
     </>
   );
 };
